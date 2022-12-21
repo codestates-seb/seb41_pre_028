7b53e41d770e5847bc91e5d4eb8ad3cc43e65c72
frontLine-kim <57103133+frontLine-kim@users.noreply.github.com>
2022-12-21 17:07:27 +0900

    JWT 활용 인증 성공 핸들러 구현

---
+++ server/src/main/java/com/codestates/pre_028/stackoverflow_clone/Auth/handler/UserAuthenticationSuccessHandler.java
@@ -0,0 +1,18 @@
+package com.codestates.pre_028.stackoverflow_clone.Auth.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+@Slf4j
+public class UserAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        log.info(" # 인증 성공입니다!");
+    }
+}
