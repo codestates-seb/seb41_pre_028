0f84a5daa3443a535a116aefe1a93613d34bb395
frontLine-kim <57103133+frontLine-kim@users.noreply.github.com>
2022-12-21 17:04:55 +0900

    JWT 적용하여 로그인 인증 및 자격 증명 검증 등 구현 / 대부분 UR-CLASS 를 참고

--- server/src/main/java/com/codestates/pre_028/stackoverflow_clone/Config/SecurityConfiguration.java
+++ server/src/main/java/com/codestates/pre_028/stackoverflow_clone/Config/SecurityConfiguration.java
@@ -1,29 +1,66 @@
 package com.codestates.pre_028.stackoverflow_clone.Config;
 
+import com.codestates.pre_028.stackoverflow_clone.Auth.Jwt.JwtTokenizer;
+import com.codestates.pre_028.stackoverflow_clone.Auth.filter.JwtAuthenticationFilter;
+import com.codestates.pre_028.stackoverflow_clone.Auth.filter.JwtVerificationFilter;
+import com.codestates.pre_028.stackoverflow_clone.Auth.handler.UserAuthenticationSuccessHandler;
+import com.codestates.pre_028.stackoverflow_clone.Auth.utils.CustomAuthorityUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils customAuthorityUtils;
+
+    public SecurityConfiguration(JwtTokenizer jwtTokenizer, CustomAuthorityUtils customAuthorityUtils) {
+        this.jwtTokenizer = jwtTokenizer;
+        this.customAuthorityUtils = customAuthorityUtils;
+    }
 
     @Bean
-     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
-                .securityMatcher("/h2**")
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/h2/**","favicon.ico").permitAll()
-                        .anyRequest()
-                        .authenticated()
-                );
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+        // H2 DB 사용 가능하게 설정
+        http.headers().frameOptions().sameOrigin()
+                .and()
+                .csrf().disable()
+                .cors(Customizer.withDefaults())
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll());// 역할에 다른 기능 추가
+
 
         return http.build();
     }
-}
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
+            jwtAuthenticationFilter.setAuthenticationSuccessHandler(new UserAuthenticationSuccessHandler());
+            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, customAuthorityUtils);
+//            jwtAuthenticationFilter.setFilterProcessesUrl("user/login");  // -> /login
+
+            builder
+                    .addFilter(jwtAuthenticationFilter)
+                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
+        }
+    }
+}
