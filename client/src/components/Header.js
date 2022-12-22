b76d75d4840e84fe99b9fdedc6a584c9d9b16f95
chyerin802 <chyerin802@gmail.com>
2022-12-22 16:26:51 +0900

    feat: Search Bar 구성

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import SearchBar from "./SearchBar";
 import { PrimaryLink, SecondaryLink } from "./StyledLink";
 
 const HeaderContainer = styled.div`
@@ -11,20 +12,6 @@ const HeaderContainer = styled.div`
   width: var(--screen-full);
 `;
 
-const SearchForm = styled.form`
-  flex-grow: 1;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  div {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    background-color: white;
-  }
-`;
-
 const Header = () => {
   return (
     <header className="fixed w-screen h-header-height bg-[#F8F9F9] flex justify-center">
@@ -32,12 +19,9 @@ const Header = () => {
         <Link to={"/"} className="h-full flex items-center justify-center">
           <span className="logo-img h-[30px] w-[150px] mt-[-4px]"></span>
         </Link>
-        <SearchForm>
-          <div>
-            <span>search 아이콘</span>
-            <input type="text" placeholder="Search..."></input>
-          </div>
-        </SearchForm>
+        <div>
+          <SearchBar></SearchBar>
+        </div>
         {/* <ul className="flex flex-row">
           <li>프로필</li>
           <li>로그아웃</li>
