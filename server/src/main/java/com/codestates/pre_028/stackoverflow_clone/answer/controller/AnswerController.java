e188a2a9154a0e6b8959c060333a1257515da881
bum19 <1996joon@gmail.com>
2022-12-22 21:41:53 +0900

    Answer 컨트롤러 수정/ 응답 dto SingleReponseDto, MultiResponseDto로 감싸줌. MultiResponseDto에 PageInfo 주석처리

--- server/src/main/java/com/codestates/pre_028/stackoverflow_clone/answer/controller/AnswerController.java
+++ server/src/main/java/com/codestates/pre_028/stackoverflow_clone/answer/controller/AnswerController.java
@@ -1,5 +1,7 @@
 package com.codestates.pre_028.stackoverflow_clone.answer.controller;
 
+import com.codestates.pre_028.stackoverflow_clone.Dto.MultiResponseDto;
+import com.codestates.pre_028.stackoverflow_clone.Dto.SingleResponseDto;
 import com.codestates.pre_028.stackoverflow_clone.answer.dto.AnswerDto;
 import com.codestates.pre_028.stackoverflow_clone.answer.entity.Answer;
 import com.codestates.pre_028.stackoverflow_clone.answer.mapper.AnswerMapper;
@@ -31,7 +33,7 @@ public class AnswerController {
 //        Answer answer = answerService.createAnswer(mapper.answerPostDtoToAnswer(answerPostDto));
 //        AnswerDto.Response response = mapper.answerToAnswerResponseDto(answer);
         AnswerDto.Response response = new AnswerDto.Response(1,1,1,"stub data", Answer.AnswerStatus.ANSWER_NORMAL,1, LocalDateTime.now(),LocalDateTime.now(),"stubUser","stubUser");
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+        return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.CREATED);
     }
 
     @PatchMapping("/{id}")
@@ -41,16 +43,17 @@ public class AnswerController {
 //        Answer answer = answerService.updateAnswer(mapper.answerPatchDtoToAnswer(answerPatchDto));
 //        AnswerDto.Response response = mapper.answerToAnswerResponseDto(answer);
         AnswerDto.Response response = new AnswerDto.Response(answerId,1,1,"stub data", Answer.AnswerStatus.ANSWER_NORMAL,1, LocalDateTime.now(),LocalDateTime.now(),"stubUser","stubUser");
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.OK);
     }
 
     @PatchMapping("/{id}/answer_vote")
     public ResponseEntity voteAnswer(@PathVariable("id") @Positive long answerId,
                                      @Valid @RequestBody AnswerDto.VotePatch votePatchDto){
-        //투표를 +-로 Vote_status를 정함.
+        //mapping하면서 vote객체하나 생성됨
+        //생성된 vote를 저장하기위해
         //Answer answer = answerService.updateVoteAnswer(answer);
         AnswerDto.Response response = new AnswerDto.Response(answerId,1,1,"stub data", Answer.AnswerStatus.ANSWER_NORMAL,1, LocalDateTime.now(),LocalDateTime.now(),"stubUser","stubUser");
-        return new ResponseEntity<>(response,HttpStatus.OK);
+        return new ResponseEntity<>(new SingleResponseDto<>(response),HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
@@ -58,14 +61,14 @@ public class AnswerController {
 
         //Answer answer = answerService.findAnswer(answerId);
         AnswerDto.Response response = new AnswerDto.Response(answerId,1,1,"stub data", Answer.AnswerStatus.ANSWER_NORMAL,1, LocalDateTime.now(),LocalDateTime.now(),"stubUser","stubUser");
-        return new ResponseEntity<>(response,HttpStatus.OK);
+        return new ResponseEntity<>(new SingleResponseDto<>(response),HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity getAnswers(){
         AnswerDto.Response response1 = new AnswerDto.Response(1,1,1,"stub data1", Answer.AnswerStatus.ANSWER_NORMAL,1, LocalDateTime.now(),LocalDateTime.now(),"stubUser1","stubUser1");
         AnswerDto.Response response2 = new AnswerDto.Response(2,2,2,"stub data2", Answer.AnswerStatus.ANSWER_NORMAL,1, LocalDateTime.now(),LocalDateTime.now(),"stubUser2","stubUser2");
-        return new ResponseEntity<>(Arrays.asList(response1,response2),HttpStatus.OK);
+        return new ResponseEntity<>(new MultiResponseDto<>(Arrays.asList(response1,response2)),HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
