873778348fccf5247975718aca5e4fc253aabaac
bakjonghyo <cutitout52@gmail.com>
2022-12-22 15:42:00 +0900

    inputPrimary 제작

---
+++ client/src/components/input/inputPrimary.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const InputPrimary = () => {
+  const [title, setTitle] = useState("");
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        id="title"
+        name="title"
+        type="text"
+        value={title}
+        maxLength="300"
+        placeholder="제목을 입력하세요"
+        className="w-full px-4 py-2 border rounded border-soGray-light"
+        data-min-length="15"
+        data-max-length="150"
+        onChange={onChangeTitle}
+      />
+      <div className="flex flex-column items-center">{title}</div>
+    </div>
+  );
+};
+
+export default InputPrimary;
