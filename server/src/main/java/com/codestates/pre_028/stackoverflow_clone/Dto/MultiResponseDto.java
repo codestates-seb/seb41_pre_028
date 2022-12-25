eb198c6bfb915f5c91a063abe6b63809cb0670b1
bum19 <1996joon@gmail.com>
2022-12-26 00:25:49 +0900

    AnswerService,AnswerRepository 구현

--- server/src/main/java/com/codestates/pre_028/stackoverflow_clone/Dto/MultiResponseDto.java
+++ server/src/main/java/com/codestates/pre_028/stackoverflow_clone/Dto/MultiResponseDto.java
@@ -9,11 +9,11 @@ import java.util.List;
 @Getter
 public class MultiResponseDto<T> {
     private List<T> data;
-//    private PageInfo pageInfo;
+    private PageInfo pageInfo;
 
-    public MultiResponseDto(List<T> data) {
+    public MultiResponseDto(List<T> data, Page page) {
         this.data = data;
-//        this.pageInfo = new PageInfo(page.getNumber() + 1,
-//                page.getSize(), page.getTotalElements(), page.getTotalPages());
+        this.pageInfo = new PageInfo(page.getNumber() + 1,
+                page.getSize(), page.getTotalElements(), page.getTotalPages());
     }
 }
--- server/src/main/java/com/codestates/pre_028/stackoverflow_clone/answer/controller/AnswerController.java
+++ server/src/main/java/com/codestates/pre_028/stackoverflow_clone/answer/controller/AnswerController.java
@@ -5,9 +5,11 @@ import com.codestates.pre_028.stackoverflow_clone.Dto.SingleResponseDto;
 import com.codestates.pre_028.stackoverflow_clone.answer.dto.AnswerDto;
 import com.codestates.pre_028.stackoverflow_clone.answer.entity.Answer;
 import com.codestates.pre_028.stackoverflow_clone.answer.mapper.AnswerMapper;
+import com.codestates.pre_028.stackoverflow_clone.answer.service.AnswerService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/answers")
@@ -22,48 +25,52 @@ import java.util.Arrays;
 @Slf4j
 public class AnswerController {
     private final AnswerMapper mapper;
+    private final AnswerService answerService;
 
-    public AnswerController(AnswerMapper mapper){
+    public AnswerController(AnswerMapper mapper, AnswerService answerService){
         this.mapper = mapper;
+        this.answerService = answerService;
     }
 
     @PostMapping
     public ResponseEntity postAnswer(@Valid @RequestBody AnswerDto.Post answerPostDto){
+        Answer answer = answerService.createAnswer(mapper.answerPostDtoToAnswer(answerPostDto));
 
-//        Answer answer = answerService.createAnswer(mapper.answerPostDtoToAnswer(answerPostDto));
-//        AnswerDto.Response response = mapper.answerToAnswerResponseDto(answer);
-        AnswerDto.Response response = new AnswerDto.Response(1,1,1,"stub data", Answer.AnswerStatus.ANSWER_NORMAL, LocalDateTime.now(),LocalDateTime.now(),"stubUser","stubUser");
-        return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.CREATED);
+        return new ResponseEntity<>(new SingleResponseDto<>(mapper.answerToAnswerResponseDto(answer)),
+                HttpStatus.CREATED);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity patchAnswer(@PathVariable("id") @Positive long answerId,
                                       @Valid @RequestBody AnswerDto.Patch answerPatchDto){
-//        answerPatchDto.setAnswerId(answerId);
-//        Answer answer = answerService.updateAnswer(mapper.answerPatchDtoToAnswer(answerPatchDto));
-//        AnswerDto.Response response = mapper.answerToAnswerResponseDto(answer);
-        AnswerDto.Response response = new AnswerDto.Response(answerId,1,1,"stub data", Answer.AnswerStatus.ANSWER_NORMAL, LocalDateTime.now(),LocalDateTime.now(),"stubUser","stubUser");
-        return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.OK);
+        answerPatchDto.setAnswerId(answerId);
+        Answer answer = answerService.updateAnswer(mapper.answerPatchDtoToAnswer(answerPatchDto));
+        return new ResponseEntity<>(new SingleResponseDto<>(mapper.answerToAnswerResponseDto(answer)),
+                HttpStatus.OK);
     }
-
 
     @GetMapping("/{id}")
     public ResponseEntity getAnswer(@PathVariable("id") @Positive long answerId){
 
-        //Answer answer = answerService.findAnswer(answerId);
-        AnswerDto.Response response = new AnswerDto.Response(answerId,1,1,"stub data", Answer.AnswerStatus.ANSWER_NORMAL, LocalDateTime.now(),LocalDateTime.now(),"stubUser","stubUser");
-        return new ResponseEntity<>(new SingleResponseDto<>(response),HttpStatus.OK);
+        Answer answer = answerService.findAnswer(answerId);
+        return new ResponseEntity<>(new SingleResponseDto<>(mapper.answerToAnswerResponseDto(answer)),
+                HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity getAnswers(){
-        AnswerDto.Response response1 = new AnswerDto.Response(1,1,1,"stub data1", Answer.AnswerStatus.ANSWER_NORMAL, LocalDateTime.now(),LocalDateTime.now(),"stubUser1","stubUser1");
-        AnswerDto.Response response2 = new AnswerDto.Response(2,2,2,"stub data2", Answer.AnswerStatus.ANSWER_NORMAL, LocalDateTime.now(),LocalDateTime.now(),"stubUser2","stubUser2");
-        return new ResponseEntity<>(new MultiResponseDto<>(Arrays.asList(response1,response2)),HttpStatus.OK);
+    public ResponseEntity getAnswers(@Positive @RequestParam int page,
+                                     @Positive @RequestParam int size){
+        Page<Answer> pageAnswers = answerService.findAnswers(page-1, size);
+        List<Answer> answers = pageAnswers.getContent();
+
+        return new ResponseEntity<>(new MultiResponseDto<>(mapper.answersToAnswerResponseDtos(answers),
+                pageAnswers),
+                HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteAnswer(){
+    public ResponseEntity deleteAnswer(@PathVariable("id") Long answerId){
+        answerService.deleteAnswer(answerId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
