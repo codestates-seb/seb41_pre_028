0a146a066a8c2c5356b2e50cb65ab83bb6546069
frontLine-kim <57103133+frontLine-kim@users.noreply.github.com>
2022-12-27 12:50:45 +0900

    거의 구현

--- server/src/main/java/com/codestates/pre_028/stackoverflow_clone/Question/controller/QuestionController.java
+++ server/src/main/java/com/codestates/pre_028/stackoverflow_clone/Question/controller/QuestionController.java
@@ -1,10 +1,14 @@
 package com.codestates.pre_028.stackoverflow_clone.Question.controller;
 
+import com.codestates.pre_028.stackoverflow_clone.Dto.MultiResponseDto;
 import com.codestates.pre_028.stackoverflow_clone.Question.Dto.QuestionDto;
+import com.codestates.pre_028.stackoverflow_clone.Question.Dto.QuestionPaginationDto;
 import com.codestates.pre_028.stackoverflow_clone.Question.entity.Question;
 import com.codestates.pre_028.stackoverflow_clone.Question.mapper.QuestionMapper;
 import com.codestates.pre_028.stackoverflow_clone.Question.service.QuestionService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -24,7 +28,6 @@ public class QuestionController {
         this.questionService = questionService ;
         this.mapper = mapper;
     }
-
     //질문 등록
     @PostMapping
     public ResponseEntity postQuestion(@RequestBody QuestionDto.QuestionPostDto questionDto){
@@ -54,17 +57,18 @@ public class QuestionController {
     }
 
     //질문 전체 조회
-//    @GetMapping
-//    public ResponseEntity getQuestions(){
-//
-//        List<Question> questions = questionService.findQuestions();
-//
-//        List<QuestionDto.QuestionResponseDto> response = questions.stream()
-//                .map(question -> mapper.questionToQuestionResponseDto(question))
-//                .collect(Collectors.toList());
-//
-//        return new ResponseEntity<>(response,HttpStatus.OK);
-//    }
+    @GetMapping
+    public ResponseEntity getQuestionPagination(@Positive @RequestParam int page,
+                                                @Positive @RequestParam int size){
+
+        Page<Question> pageQuestions = questionService.findQuestions(page - 1, size);
+        List<Question> questions = pageQuestions.getContent();
+        List<QuestionPaginationDto> responseDtos = mapper.questionToQuestionResponseDto(questions);
+
+        return new ResponseEntity<>(
+                new MultiResponseDto<>(responseDtos, pageQuestions), HttpStatus.OK
+        );
+    }
 
     //질문 삭제
     @DeleteMapping("/{question-id}")
