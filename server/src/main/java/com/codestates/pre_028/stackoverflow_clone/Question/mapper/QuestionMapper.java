dd573ec5d95b939bfbab843480c2afddc5b3525c
bum19 <1996joon@gmail.com>
2022-12-28 21:10:33 +0900

    Quesetion -> QuestionWithAnswerDto 변환 매퍼에 title 셋 코드 추가

--- server/src/main/java/com/codestates/pre_028/stackoverflow_clone/Question/mapper/QuestionMapper.java
+++ server/src/main/java/com/codestates/pre_028/stackoverflow_clone/Question/mapper/QuestionMapper.java
@@ -36,6 +36,7 @@ public interface QuestionMapper {
 
     };
     Question questionPatchDtoToQuestion(QuestionDto.QuestionPatchDto questionPatchDto);
+
     default QuestionWithAnswerDto questionToQuestionResponseDto(Question question){
         List<Comment> comments = question.getComments();
         List<Answer> answers = question.getAnswerList();
@@ -44,6 +45,7 @@ public interface QuestionMapper {
         questionResponseDto.setQuestionId(question.getQuestionId());
         questionResponseDto.setContent(question.getContent());
         questionResponseDto.setUser(question.getUser());
+        questionResponseDto.setTitle(question.getTitle());
         questionResponseDto.setTag(question.getTag());
 
         //메타데이타
