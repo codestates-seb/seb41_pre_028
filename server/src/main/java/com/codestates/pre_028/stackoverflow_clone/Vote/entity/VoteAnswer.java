72af6bb31c34f833c714cadde26e65c677e788bb
bum19 <1996joon@gmail.com>
2022-12-29 12:43:36 +0900

    VoteAnswer, VoteAnswerDto 생성

---
+++ server/src/main/java/com/codestates/pre_028/stackoverflow_clone/Vote/entity/VoteAnswer.java
@@ -0,0 +1,34 @@
+package com.codestates.pre_028.stackoverflow_clone.Vote.entity;
+
+import com.codestates.pre_028.stackoverflow_clone.User.entity.User;
+import com.codestates.pre_028.stackoverflow_clone.answer.entity.Answer;
+import com.codestates.pre_028.stackoverflow_clone.comment.entity.Comment;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+public class VoteAnswer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long voteAnswerId;
+
+    private Long voteNum = 0L;
+
+    @OneToOne
+    @JoinColumn(name = "ANSWER_ID")
+    private Answer answer;
+
+    @ElementCollection
+    private List<Long> userIds = new ArrayList<>();
+
+    public void setUserIds(Long userId){
+        this.userIds.add(userId);
+    }
+}
---
+++ server/src/main/java/com/codestates/pre_028/stackoverflow_clone/answer/dto/VoteAnswerDto.java
@@ -0,0 +1,23 @@
+package com.codestates.pre_028.stackoverflow_clone.answer.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VoteAnswerDto {
+
+    Long answerId;
+
+    @NotNull
+    @Positive
+    Long userId;
+
+    //1 or -1만
+    @NotNull
+    Long vote;
+
+}
