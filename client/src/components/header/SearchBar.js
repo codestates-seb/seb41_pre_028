92ec8d6000b6143b302d2480d8c54b4ec8d7d2f1
chyerin802 <chyerin802@gmail.com>
2022-12-31 22:57:32 +0900

    feat: 페이지 이동 시 서치바 초기화

--- client/src/components/header/SearchBar.js
+++ client/src/components/header/SearchBar.js
@@ -1,9 +1,10 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { change, focus, blur } from "../../store/searchSlice";
+import { useNavigate, useLocation } from "react-router-dom";
+import { change, focus, blur, reset } from "../../store/searchSlice";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from "react";
 
 const SearchInput = styled.input`
   width: 100%;
@@ -25,13 +26,38 @@ const SearchModal = styled.div`
   top: var(--h-header);
   transform: translate(-50%, 0%);
   width: 100%;
+  padding: 10px;
+  gap: 1rem;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+
+  > div {
+    > span:first-child {
+      font-size: 1rem;
+      margin-right: 0.5rem;
+      font-weight: 600;
+    }
+    > span:last-child {
+      font-size: 0.8rem;
+      color: #5e666e;
+    }
+  }
 `;
 
 const SearchBar = () => {
   // slice를 생성할 때 지정해준 이름을 이용해 state를 이용한다.
+  const { pathname } = useLocation();
   const { word, focused } = useSelector((state) => state.search);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (pathname !== "/search") {
+      dispatch(reset());
+    }
+  }, [pathname]);
 
   const handleKeyUp = (e) => {
     if (e.key === "Enter") {
@@ -50,9 +76,10 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="w-full pr-[5px] pl-[5px] max-h-max relative rounded-[3px]">
+    <div className="w-[100%] pr-[5px] pl-[5px] max-h-max relative rounded-[3px]">
       <SearchInput
         type="text"
+        value={word}
         placeholder="Search..."
         onKeyUp={handleKeyUp}
         onChange={handleChange}
