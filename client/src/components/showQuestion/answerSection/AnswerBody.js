cd226be29cf79f756dcd2b5ea42a9d81859b8c05
bakjonghyo <cutitout52@gmail.com>
2023-01-01 20:04:47 +0900

    #91 ShowQuestion CSS 수정

--- client/src/components/showQuestion/answerSection/AnswerBody.js
+++ client/src/components/showQuestion/answerSection/AnswerBody.js
@@ -1,10 +1,12 @@
 import VoteCell from "../VoteCell";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BufferMd5 } from "../../buffer/Buffer";
 import styled from "styled-components";
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import CommentAnswerCell from "../commentSection/CommentAnswerCell";
-
+import { isCookieExist } from "../../../utils/cookie";
+import { getMyProfile } from "../../../utils/api/api";
+import axios from "axios";
 const QuestionStats = styled.div`
   display: flex;
   flex-direction: row;
@@ -16,78 +18,122 @@ const QuestionStats = styled.div`
 `;
 
 const AnswerBody = ({ item }) => {
-  // const [comments, setQuestion] = useState([]);
-  // const getData = async () => {
-  //   await fetch(`/answers/${answerId}/comments`)
-  //     .then((response) => response.json())
-  //     .then((data) => setQuestion(data))
-  //     .catch((error) => console.log(error));
-  // };
+  const [userId, setUserId] = useState(false);
+  const [editModeOn, setEditModeOn] = useState(false);
 
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+  const navigate = useNavigate();
 
-  // const { answerId } = useParams();
+  useEffect(() => {
+    const SetupUserPage = () => {
+      if (isCookieExist) {
+        getMyProfile()
+          .then((res) => {
+            setUserId(res.data.userId);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    };
+
+    SetupUserPage();
+  }, [userId]);
+
+  const onClickDeleteAnswers = () => {
+    axios
+      .delete(`/answers/${item.answerId}`, {
+        headers: {
+          Authorization: isCookieExist,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        navigate("/questions");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="flex flex-col">
-      <div className="m-4 flex flex-row">
+      <div className="m-4  flex flex-row w-full">
         <VoteCell item={item.userId} />
-        <div> {item.content}</div>
+        <div className="flex flex-col w-full">
+          <div className="flex flex-row w-full">{item.content}</div>
+          <BufferMd5></BufferMd5>
+          {/* profile */}
+          <div className="flex flex-row items-center space-between ml-auto">
+            {!(userId === item.userId) ? (
+              <QuestionStats>
+                <div>
+                  <Link
+                    to={`/questions/${item.questionId}`}
+                    className="text-[#6a737C]"
+                  >
+                    Share
+                  </Link>
+                </div>
+                <div>
+                  <Link
+                    to={`/questions/${item.questionId}`}
+                    className="text-[#6a737C]"
+                  >
+                    Follow
+                  </Link>
+                </div>
+              </QuestionStats>
+            ) : (
+              <QuestionStats>
+                <div>
+                  <Link
+                    to={`/questions/${item.questionId}`}
+                    className="text-[#6a737C]"
+                  >
+                    Share
+                  </Link>
+                </div>
+                <div>
+                  <button onClick={() => setEditModeOn(!editModeOn)}>
+                    Edit
+                  </button>
+                </div>
+                <div>
+                  <button
+                    className="text-[#6a737C]"
+                    onClick={onClickDeleteAnswers}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </QuestionStats>
+            )}
 
-        {/* <div> {JSON.stringify(item)}</div> */}
-      </div>
-      <BufferMd5 />
-      {/* tag */}
-
-      {/* profile */}
-      <div className="flex flex-row items-center  ml-auto">
-        <QuestionStats>
-          <div>
-            <Link
-              to={`/questions/${item.questionId}`}
-              className="text-[#6a737C]"
-            >
-              Share
-            </Link>
-          </div>
-          <div>
-            <Link to={`/questions/create`} className="text-[#6a737C]">
-              Edit
-            </Link>
-          </div>
-          <div>
-            <Link
-              to={`/questions/${item.questionId}`}
-              className="text-[#6a737C]"
-            >
-              Follow
-            </Link>
-          </div>
-        </QuestionStats>
-
-        <Link to={`/users/${item.userId}`}>
-          <div className="flex items-center justify-center">
-            <img
-              className="block w-[32px] h-[32px] rounded-[3px]"
-              src="https://www.gravatar.com/avatar/adef0b5893a6615076a5b41cbbcfc7bc?s=256&d=identicon&r=PG"
-              alt={`${item.nickname}'s avatar`}
-            />
-          </div>
-        </Link>
-        <div className="flex flex-col">
-          <div className="text-[12px] text-[#6a637c]">
-            asked at {item.createdAt}
-          </div>
-          <div className="h-full items-center">
-            <div className="flex items-center text-[12px] text-[#0063bf]">
-              <Link to={`/users/${item.userId}`}>{item.userId}</Link>
+            {/* profile */}
+            <div className="flex flex-row items-center  ml-auto">
+              <Link to={`/users/${item.userId}`}>
+                <div className="flex items-center justify-center">
+                  <img
+                    className="block w-[32px] h-[32px] rounded-[3px]"
+                    src="https://www.gravatar.com/avatar/adef0b5893a6615076a5b41cbbcfc7bc?s=256&d=identicon&r=PG"
+                    alt={`${item.nickname}'s avatar`}
+                  />
+                </div>
+              </Link>
+              <div className="flex flex-col">
+                <div className="text-[12px] text-[#6a637c]">
+                  asked at {item.createdAt}
+                </div>
+                <div className="h-full items-center">
+                  <div className="flex items-center text-[12px] text-[#0063bf]">
+                    <Link to={`/users/${item.userId}`}>{item.nickname}</Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+          {/* comment */}
+          <CommentAnswerCell answerId={item.answerId} />
         </div>
       </div>
-      {/* comment */}
-      <CommentAnswerCell answerId={item.answerId} />
     </div>
   );
 };
--- client/src/components/showQuestion/questionsection/QuestionBody.js
+++ client/src/components/showQuestion/questionsection/QuestionBody.js
@@ -5,6 +5,9 @@ import CommentCell from "../commentSection/CommentCell";
 import styled from "styled-components";
 import { isCookieExist } from "../../../utils/cookie";
 import axios from "axios";
+import Tag from "../../StyledTag";
+import { useState, useEffect } from "react";
+import { getQuestionDetail, getMyProfile } from "../../../utils/api/api";
 const QuestionStats = styled.div`
   display: flex;
   flex-direction: row;
@@ -16,7 +19,37 @@ const QuestionStats = styled.div`
 `;
 
 const QuestionBody = ({ item }) => {
+  // const userId = item.userId;
+  // console.log("userId", userId);
+  const [userId, setUserId] = useState(false);
+  const [question, setQuestion] = useState({
+    tagList: [],
+  });
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const SetupUserPage = () => {
+      if (isCookieExist) {
+        getMyProfile()
+          .then((res) => {
+            setUserId(res.data.userId);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    };
+
+    SetupUserPage();
+  }, [userId]);
+
+  getQuestionDetail(item.questionId)
+    .then((res) => {
+      setQuestion(res.data.data);
+    })
+    .catch((err) => console.log(err));
+
   const onClickDelete = () => {
     axios
       .delete(`/questions/${item.questionId}`, {
@@ -36,73 +69,96 @@ const QuestionBody = ({ item }) => {
       {/* screen 크기에 맞추기 */}
       <div className="m-4  flex flex-row w-full">
         <VoteCell item={item.userId} />
-        <div className="flex flex-row w-full"> {item.content}</div>
-      </div>
-      <BufferMd5 />
-      {/* tag */}
-      {/* profile */}
-      <div className="flex flex-row items-center space-between ml-auto">
-        <QuestionStats>
-          <div>
-            <Link
-              to={`/questions/${item.questionId}`}
-              className="text-[#6a737C]"
-            >
-              Share
-            </Link>
+        <div className="flex flex-col w-full">
+          <div className="flex flex-row w-full"> {item.content}</div>
+          <BufferMd5 />
+          {/* tag */}
+          <div className="flex flex-row">
+            {question.tagList.map((tag, idx) => (
+              <Tag onClick={() => navigate(`/search?value=[${tag}]`)} key={idx}>
+                {tag}
+              </Tag>
+            ))}
           </div>
-          <div>
-            <Link
-              to={`/questions/${item.questionId}/edit`}
-              state={{
-                questionId: item.questionId,
-                oldTitle: item.title,
-                oldTags: item.tags,
-                oldContent: item.content,
-              }}
-              className="text-[#6a737C]"
-            >
-              Edit
-            </Link>
-            <button> </button>
-          </div>
-          <div>
-            <Link
-              to={`/questions/${item.questionId}`}
-              className="text-[#6a737C]"
-            >
-              Follow
-            </Link>
-          </div>
-          <div>
-            <button className="text-[#6a737C]" onClick={onClickDelete}>
-              Delete
-            </button>
-          </div>
-        </QuestionStats>
-
-        <Link to={`/users/${item.userId}`}>
-          <div className="flex items-center justify-center">
-            <img
-              className="block w-[32px] h-[32px] rounded-[3px]"
-              src="https://www.gravatar.com/avatar/adef0b5893a6615076a5b41cbbcfc7bc?s=256&d=identicon&r=PG"
-              alt={`${item.nickname}'s avatar`}
-            />
-          </div>
-        </Link>
-        <div className="flex flex-col">
-          <div className="text-[12px] text-[#6a637c]">
-            asked at {item.createdAt}
-          </div>
-          <div className="h-full items-center">
-            <div className="flex items-center text-[12px] text-[#0063bf]">
-              <Link to={`/users/${item.userId}`}>{item.createdBy}</Link>
+          {/* profile */}
+          <div className="flex flex-row w-max space-between ml-auto">
+            {!(userId === item.userId) ? (
+              <QuestionStats>
+                <div>
+                  <Link
+                    to={`/questions/${item.questionId}`}
+                    className="text-[#6a737C]"
+                  >
+                    Share
+                  </Link>
+                </div>
+                <div>
+                  <Link
+                    to={`/questions/${item.questionId}`}
+                    className="text-[#6a737C]"
+                  >
+                    Follow
+                  </Link>
+                </div>
+              </QuestionStats>
+            ) : (
+              <QuestionStats>
+                <div>
+                  <Link
+                    to={`/questions/${item.questionId}`}
+                    className="text-[#6a737C]"
+                  >
+                    Share
+                  </Link>
+                </div>
+                <div>
+                  <Link
+                    to={`/questions/${item.questionId}/edit`}
+                    state={{
+                      questionId: item.questionId,
+                      oldTitle: item.title,
+                      oldTags: item.tags,
+                      oldContent: item.content,
+                    }}
+                    className="text-[#6a737C]"
+                  >
+                    Edit
+                  </Link>
+                </div>
+                <div>
+                  <button className="text-[#6a737C]" onClick={onClickDelete}>
+                    Delete
+                  </button>
+                </div>
+              </QuestionStats>
+            )}
+            <div className="flex flex-row items-center  ml-auto">
+              <Link to={`/users/${item.userId}`}>
+                <div className="flex items-center justify-center">
+                  <img
+                    className="block w-[32px] h-[32px] rounded-[3px]"
+                    src="https://www.gravatar.com/avatar/adef0b5893a6615076a5b41cbbcfc7bc?s=256&d=identicon&r=PG"
+                    alt={`${item.nickname}'s avatar`}
+                  />
+                </div>
+              </Link>
+              <div className="flex flex-col">
+                <div className="text-[12px] text-[#6a637c]">
+                  asked at {item.createdAt}
+                </div>
+                <div className="h-full items-center">
+                  <div className="flex items-center text-[12px] text-[#0063bf]">
+                    <Link to={`/users/${item.userId}`}>{item.nickname}</Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+          {/* comment */}
+          <CommentCell comments={item.comments} questionId={item.questionId} />
         </div>
       </div>
-      {/* comment */}
-      <CommentCell comments={item.comments} questionId={item.questionId} />
+      <BufferMd5 />
     </div>
   );
 };
