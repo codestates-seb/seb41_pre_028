095c27b28912c207cfac82b541cf5eb578ea9141
chyerin802 <chyerin802@gmail.com>
2023-01-02 16:28:22 +0900

    FE last

--- server/src/main/java/com/codestates/pre_028/stackoverflow_clone/Question/repository/querydsl/QuestionRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package com.codestates.pre_028.stackoverflow_clone.Question.repository.querydsl;
-
-import java.util.List;
-
-public interface QuestionRepositoryCustom {
-    List<String> findAllDistinctContent();
-}
